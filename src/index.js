1bcee4dd028fc37e9d12f4ff53c48044b3c91950
Luybov Kurij <lubovhimicka@gmail.com>
2023-10-12 15:39:21 +0300

    feat: add switch

--- src/index.js
+++ src/index.js
@@ -1,54 +1,115 @@
 module.exports = function toReadable (number) {
-const numbersToWords = {
-      0: "zero",
-      1: "one",
-      2: "two",
-      3: "three",
-      4: "four",
-      5: "five",
-      6: "six",
-      7: "seven",
-      8: "eight",
-      9: "nine",
-      10: "ten",
-      11: "eleven",
-      12: "twelve",
-      13: "thirteen",
-      14: "fourteen",
-      15: "fifteen",
-      16: "sixteen",
-      17: "seventeen",
-      18: "eighteen",
-      19: "nineteen",
-      20: "twenty",
-      30: "thirty",
-      40: "forty",
-      50: "fifty",
-      60: "sixty",
-      70: "seventy",
-      80: "eighty",
-      90: "ninety",
-    };
-    function getWorld(number) {
-      if (number in numbersToWords) return numbersToWords[number];
 
-      let words = "";
+    let word = " ";
+    function getWordWithOneNumber(number){
+
+    if (number === 0){
+       word = 'zero';
+    }
+
+    switch (number) {
+        case 1:
+            word = "one";
+            break;
+        case 2:
+            word = "two";
+            break;
+        case 3:
+            word = "three";
+            break;
+        case 4:
+            word = "four";
+            break;
+        case 5:
+            word = "five";
+            break;
+        case 6:
+            word = "six";
+            break;
+        case 7:
+            word = "seven";
+            break;
+        case 8:
+            word = "eight";
+            break;
+        case 9:
+            word = "nine";
+            break;
+        case 10:
+            word = "ten";
+            break;
+        case 11:
+            word = "eleven";
+            break;
+        case 12:
+            word = "twelve";
+            break;
+        case 13:
+            word = "thirteen";
+            break;
+        case 14:
+            word = "fourteen";
+            break;
+        case 15:
+            word = "fifteen";
+            break;
+        case 16:
+            word = "sixteen";
+            break;
+        case 17:
+            word = "seventeen";
+            break;
+        case 18:
+            word = "eighteen";
+            break;
+        case 19:
+            word = "nineteen";
+            break;
+        case 20:
+            word = "twenty";
+            break;
+        case 30:
+            word = "thirty";
+            break;
+        case 40:
+            word = "forty";
+            break;
+        case 50:
+            word = "fifty";
+            break;
+        case 60:
+            word = "sixty";
+            break;
+        case 70:
+            word = "seventy";
+            break;
+        case 80:
+            word = "eighty";
+            break;
+        case 90:
+            word = "ninety";
+            break;
+    }
+    return word;
+ }
+ getWordWithOneNumber(20);
+
 
       if (number >= 100) {
-          words += getWorld(Math.floor(number / 100)) + " hundred";
+          word +=toReadable( Math.floor(number / 100))+ " hundred";
           number %= 100;
       }
       if (number > 0) {
-          if (words !== "") words += " and ";
+          if (word !== "") words += " and ";
           if (number < 20) words += numbersToWords[number];
           else {
-              words += numbersToWords[Math.floor(number / 10) * 10];
+              word += numbersToWords[Math.floor(number / 10) * 10];
 
               if (number % 10 > 0) {
-                  words += "-" + numbersToWords[number % 10];
+                  word += "-" + numbersToWords[number % 10];
                 }
             }
         }
-    return words;
-}
-};
+    return word;
+    }
+
